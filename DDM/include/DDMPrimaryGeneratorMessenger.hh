6d8d94a039928ee723aa6a19d052ca2cec3b97ba
WSugg1 <sugg199@gmail.com>
2016-11-14 14:26:26 +0000

    Update and rename OpNovicePrimaryGeneratorMessenger.hh to DDMPrimaryGeneratorMessenger.hh

--- DDM/include/DDMPrimaryGeneratorMessenger.hh
+++ DDM/include/DDMPrimaryGeneratorMessenger.hh
@@ -30,28 +30,28 @@
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-#ifndef OpNovicePrimaryGeneratorMessenger_h
-#define OpNovicePrimaryGeneratorMessenger_h 1
+#ifndef DDMPrimaryGeneratorMessenger_h
+#define DDMPrimaryGeneratorMessenger_h 1
 
 #include "G4UImessenger.hh"
 #include "globals.hh"
 
-class OpNovicePrimaryGeneratorAction;
+class DDMPrimaryGeneratorAction;
 class G4UIdirectory;
 class G4UIcmdWithADoubleAndUnit;
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-class OpNovicePrimaryGeneratorMessenger: public G4UImessenger
+class DDMPrimaryGeneratorMessenger: public G4UImessenger
 {
   public:
-    OpNovicePrimaryGeneratorMessenger(OpNovicePrimaryGeneratorAction* );
-    virtual ~OpNovicePrimaryGeneratorMessenger();
+    DDMPrimaryGeneratorMessenger(DDMPrimaryGeneratorAction* );
+    virtual ~DDMPrimaryGeneratorMessenger();
  
     virtual void SetNewValue(G4UIcommand*, G4String);
  
   private:
-    OpNovicePrimaryGeneratorAction* fOpNoviceAction;
+    DDMPrimaryGeneratorAction* fDDMAction;
     G4UIdirectory*                  fGunDir;
     G4UIcmdWithADoubleAndUnit*      fPolarCmd;
 };
