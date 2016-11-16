f090040a743bc158aeb111b156af8d51728b3944
GPenn <gp13181@bristol.ac.uk>
2016-11-16 14:53:00 +0000

    Fix rotation

--- DDM/src/DDMDetectorConstruction.cc
+++ DDM/src/DDMDetectorConstruction.cc
@@ -271,8 +271,8 @@ G4VPhysicalVolume* DDMDetectorConstruction::Construct()
   G4LogicalVolume* argonTank_log
     = new G4LogicalVolume(argonTank_box,argon_gas,"Tank",0,0,0);
  
-  G4RotationMatrix rotm  = G4RotationMatrix();
-  rotm.rotateX(90*deg);
+  G4RotationMatrix* rotm  = new G4RotationMatrix();
+  rotm->rotateX(90*deg);
 
   G4VPhysicalVolume* argonTank_phys
     = new G4PVPlacement(rotm,G4ThreeVector(),argonTank_log,"Tank",
