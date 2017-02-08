6a94ab3a8747944a9875053f218f340463cd5601
Gabriel Penn <gp13181@bristol.ac.uk>
2017-02-08 14:30:22 +0000

    Change branch stuff to tree

--- DDM/include/DDMRootManager.hh
+++ DDM/include/DDMRootManager.hh
@@ -19,7 +19,7 @@ class DDMRootManager
 		TFile* root_file;
 		//Define _mng variables/arrays as needed.
 		Double_t TimeStepData_mng[5] = {0};
-		Int_t BranchCounter_mng = -1;
+		Int_t TreeCounter_mng = -1;
 		
 
 
@@ -33,11 +33,11 @@ class DDMRootManager
 		void InitialiseTree(G4String treename1);//Add more trees as arguments if desired.
 
 		//void FillTree(); Make more of these (with specific names and the required parameters) for the individual branches as needed.
-		void FillBranch_TimeStepData(G4double input_time, G4double input_x, G4double input_y, G4double input_z, G4double input_energy);
+		void FillTree_TimeStepData(G4double input_time, G4double input_x, G4double input_y, G4double input_z, G4double input_energy);
 
 		void CloseTree();
 	
-		void NewBranch();
+		//void NewBranch();
 
 		//Define Get and Set methods for each _mng variable: can be defined inline.
 
