fab13224df3ec0302fd30ca47c2430678cab14bb
Gabriel Penn <gp13181@bristol.ac.uk>
2017-02-09 15:39:53 +0000

    Add electronGen histogram

--- DDM/src/DDMRootManager.cc
+++ DDM/src/DDMRootManager.cc
@@ -3,7 +3,8 @@
 DDMRootManager* the_root_manager;
 TTree* trueTrack_tree;
 TTree* electronData_tree;
-TCanvas* c1;
+//TCanvas* c1;
+TH3I* electronGen_hist;
 
 void DDMRootManager::CreateRootManager(G4String filename)
 {
@@ -29,7 +30,7 @@ void DDMRootManager::InitialiseTrees()
 {
 	EventCounter_mng++;
 	
-	c1 = new TCanvas("c1");
+	//c1 = new TCanvas("c1");
 	
 	// True track information:
 	stringstream trueTrack_treename;
@@ -44,6 +45,7 @@ void DDMRootManager::InitialiseTrees()
 	electronData_tree = new TTree(electronData_treename.str().c_str(), electronData_treename.str().c_str());
 	
 	electronData_tree -> Branch("electronData_branch",&ElectronData_mng, "Time_ns/D:posx_m/D:posz_m/D");
+	electronGen_hist = new TH3I("electronGen_hist", "Electron generation", 100, -2.0, 2.0, 100, -2.0, 2.0, 100, -2.0, 2.0);
 }
 
 void DDMRootManager::FillTree_TimeStepData(G4double input_time, G4double input_x, G4double input_y, G4double input_z, G4double input_energy)
@@ -70,9 +72,12 @@ void DDMRootManager::CloseTrees()
 {
 	trueTrack_tree->Write();
 	electronData_tree->Write();
+	electronGen_hist->Write();
+	
 	
 	delete trueTrack_tree;
 	delete electronData_tree;
+	delete electronGen_hist;
 }
 
 DDMRootManager::~DDMRootManager()
