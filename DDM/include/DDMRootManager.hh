c560de022cd52ef1598413174dfd6d3224e1dd19
Gabriel Penn <gp13181@bristol.ac.uk>
2017-02-16 16:56:02 +0000

    Add missing type

--- DDM/include/DDMRootManager.hh
+++ DDM/include/DDMRootManager.hh
@@ -56,7 +56,7 @@ class DDMRootManager
 		void FillGraph_RecoTrackXZ(G4double input_x, G4double input_z);
 		void FillGraph_RecoTrackYZ(G4double input_y, G4double input_z);
 		void FillGraph_RecoTrack(G4double input_x, G4double input_y, G4double input_z);
-		void FillTree_RecoResults(G4double input_tanphi, G4double input_tantheta_xz, input_tantheta_yz);
+		void FillTree_RecoResults(G4double input_tanphi, G4double input_tantheta_xz, G4double input_tantheta_yz);
 	
 		void FinaliseEvent();
 		void CloseResultsTree();
