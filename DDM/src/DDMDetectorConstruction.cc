37ddc7470b6f7ec55dc35c73a97add13c6b9f126
WSugg1 <sugg199@gmail.com>
2017-02-17 11:03:20 +0000

    reduce pressure

--- DDM/src/DDMDetectorConstruction.cc
+++ DDM/src/DDMDetectorConstruction.cc
@@ -105,8 +105,8 @@ G4VPhysicalVolume* DDMDetectorConstruction::Construct()
 //
   G4Element* Ar = new G4Element("Argon", "Ar", z=18, a=39.95*g/mole);
  
-  G4double argonPressure = 0.1; // pressure in atmospheres
-  //G4double argonPressure = 0.001; // pressure in atmospheres
+  //G4double argonPressure = 0.1; // pressure in atmospheres
+  G4double argonPressure = 0.001; // pressure in atmospheres
   G4double argonDriftVelocity = 5.05e5*(cm/s);
  
   root_manager->SetDriftVelocity(argonDriftVelocity);
