16a961c610663c259d4f6b7b3f90f3e13f4e4ab7
WSugg1 <sugg199@gmail.com>
2017-02-22 16:09:13 +0000

    extend commented-out diffusion eqs

--- DDM/src/DDMSteppingAction.cc
+++ DDM/src/DDMSteppingAction.cc
@@ -174,8 +174,8 @@ void DDMSteppingAction::UserSteppingAction(const G4Step* step)
       // *****  electron diffusion  ******************************************************
       
       // put Gaussians here
-      //G4double xDiff = G4RandGauss::shoot();
-      //final_x = initial_x + xDiff
+      //final_x += G4RandGauss::shoot()*mm;
+      //final_y += G4RandGauss::shoot()*mm;
       
       // *****  filling data  ************************************************************
       
