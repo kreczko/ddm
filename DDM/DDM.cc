b83b5dac7c911c0f3ab4254814da3b698f4618bd
WSugg1 <sugg199@gmail.com>
2017-02-22 16:31:22 +0000

    comment out test of generating Gaussian numbers

--- DDM/DDM.cc
+++ DDM/DDM.cc
@@ -192,14 +192,14 @@ int main(int argc,char** argv)
   
   DDMRootManager::DestroyRootManager();
   
-  
+  /*
   // trial generating Gaussian random numbers
   for(G4int i = 0; i < 50; i++)
   {
     G4double randNum = G4RandGauss::shoot(10.0, 0.1);
     G4cout << randNum << G4endl;
   }
-  
+  */
 
   return 0;
 }
