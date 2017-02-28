830e7caba9c7cb7f4507b1bba26a5ea737177677
WSugg1 <sugg199@gmail.com>
2017-02-28 11:46:06 +0000

    typo

--- DDM/src/DDMRootManager.cc
+++ DDM/src/DDMRootManager.cc
@@ -243,7 +243,7 @@ void DDMRootManager::FitCameraHist()
 	{
 		for(G4int biny = 0; biny < camera_hist->GetNBinsY; biny++)
 		{
-			for(G4int photonPerBin = 0; photonPerBin < fitCamera_graph->GetBinContent(binx, biny); photonPerBin++)
+			for(G4int photonPerBin = 0; photonPerBin < camera_hist->GetBinContent(binx, biny); photonPerBin++)
 			{
 				G4double binCentreX = camera_hist->GetXaxis()->GetBinCenter(camera_hist->GetXaxis()->FindBin(binx));
 				G4double binCentreY = camera_hist->GetYaxis()->GetBinCenter(camera_hist->GetYaxis()->FindBin(biny));
