31567b1283fbcb077d84fd715858fee45773343f
Gabriel Penn <gp13181@bristol.ac.uk>
2017-03-08 11:36:39 +0000

    Convert pressure in name to atmospheres

--- DDM/src/DDMRootManager.cc
+++ DDM/src/DDMRootManager.cc
@@ -50,7 +50,7 @@ void DDMRootManager::CreateOutputFile(G4String filename)
 	if (filename == "[dynamic]")
 	{
 		stringstream filename_stream;
-		filename_stream << "ddm_p" << GasPressure_mng << ".root";
+		filename_stream << "ddm_p" << GasPressure_mng/atmosphere << ".root";
 		filename = filename_stream.str();
 	}
 	root_file = new TFile(filename.c_str(),"RECREATE");
