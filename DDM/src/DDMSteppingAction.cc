7861b0e8dbd8f011087624e80d17c2eecb793166
WSugg1 <sugg199@gmail.com>
2017-03-29 10:34:42 +0100

    try filling direct scint with 2 photons each time

--- DDM/src/DDMSteppingAction.cc
+++ DDM/src/DDMSteppingAction.cc
@@ -229,7 +229,11 @@ void DDMSteppingAction::UserSteppingAction(const G4Step* step)
       
       // pixellated view of photons
       if (root_manager->IsStreamliningOff())
-        {root_manager->FillHist_DirectScint(final_x, final_y, scintPhotons);}
+        {//root_manager->FillHist_DirectScint(final_x, final_y, scintPhotons);}
+      
+      root_manager->FillHist_DirectScint(final_x, final_y, 2.0);
+      
+      
       
       // propagation of photons to lens
       for (G4int j = 0; j < scintPhotons; j++)
