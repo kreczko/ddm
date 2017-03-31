912af4ed64eb3a13fd0a7379b0c7690ec0f61fb4
Gabriel Penn <gp13181@bristol.ac.uk>
2017-03-31 17:07:54 +0100

    Put data confining back in

--- DDM/src/DDMRootManager.cc
+++ DDM/src/DDMRootManager.cc
@@ -396,9 +396,10 @@ Double_t DDMRootManager::FitCameraHist(TH2I* input_hist)
 	G4cout << "Data range x: " << input_hist->FindFirstBinAbove(0, 1) << "--" << input_hist->FindLastBinAbove(0, 1) << G4endl;
 	G4cout << "Data range y: " << input_hist->FindFirstBinAbove(0, 2) << "--" << input_hist->FindLastBinAbove(0, 2) << G4endl;
 	// loop through all bins
-	for(G4int binx = 1; binx <= input_hist->GetNbinsX(); binx++)
+	//for(G4int binx = 1; binx <= input_hist->GetNbinsX(); binx++)
+	for (G4int binx = input_hist->FindFirstBinAbove(0, 1); binx <= input_hist->FindLastBinAbove(0, 1); binx++)
 	{
-		for(G4int biny = 1; biny <= input_hist->GetNbinsY(); biny++)
+		for(G4int biny = input_hist->FindFirstBinAbove(0, 2); biny <= input_hist->FindLastBinAbove(0, 2); biny++)
 		{
 			// find bin centres
 			Double_t binCentreX = input_hist->GetXaxis()->GetBinCenter(binx);
