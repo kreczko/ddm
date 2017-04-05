40fca3d92a01ba16e81ee84c1dab8115051d7d46
Gabriel Penn <gp13181@bristol.ac.uk>
2017-04-05 19:42:24 +0100

    Update DDMAnalysis.cpp

--- DDMAnalysis.cpp
+++ DDMAnalysis.cpp
@@ -1,2 +1,37 @@
 #include <iostream>
 #include <fstream>
+
+#include "TFile.h"
+#include "TTree.h"
+
+using namespace std;
+
+int main (int argc, char** argv)
+{
+  string steeringFileName = argv[1];
+  
+  ifstream steeringFile;
+  steeringFile.open(steeringFileName);
+  
+  string word;
+  string filename;
+  double pressure;
+  double timeRes;
+  
+  while (steeringFile >> word)
+  {
+    filename = word;
+    
+    steeringFile >> word;
+    pressure << atof(word);
+    
+    steeringFile >> word;
+    timeRes << atof(word);
+    
+    cout << "Reading file: " << filename << " (pressure " << pressure << " atm, time resolution " << timeRes << " ns)" << endl;
+  }
+  
+  steeringFile.close();
+  
+  return 0;
+}
