b8e47b1ea22cedf670f1c2afc3185a7f5ea87446
Gabriel Penn <gp13181@bristol.ac.uk>
2017-04-15 11:59:42 +0100

    Add plots of individual theta and phi error medians

--- DDMAnalysis.cpp
+++ DDMAnalysis.cpp
@@ -34,6 +34,12 @@ int main (int argc, char** argv)
   TGraph2D* deviationPlotMean = new TGraph2D(1);
   deviationPlotMean->SetNameTitle("deviationMean", "Mean directional deviation vs pressure and time resolution");
   
+  TGraph2D* thetaErrorPlot = new TGraph2D(1);
+  thetaErrorPlot->SetNameTitle("thetaError", "Median theta deviation vs pressure and time resolution");
+  
+  TGraph2D* phiErrorPlot = new TGraph2D(1);
+  phiErrorPlot->SetNameTitle("phiError", "Median phi deviation vs pressure and time resolution");
+  
   Int_t pointCounter = 0;
   
   while (steeringFile >> word)
@@ -42,6 +48,8 @@ int main (int argc, char** argv)
     deviationPlot->Set(pointCounter);
     
     TH1I* deviationHist = new TH1I("deviation", "deviation", 100, 0, M_PI);
+    TH1I* thetaHist = new TH1I("thetaError", "thetaError", 100, 0, M_PI);
+    TH1I* phiHist = new TH1I("phiError", "phiError", 100, 0, 2*M_PI);
     
     // Get parameters from steering file
     
@@ -72,16 +80,23 @@ int main (int argc, char** argv)
       dataTree->GetEntry(i);
       
       deviationHist->Fill(branchData[6]);
+      thetaHist->Fill(branchData[7]);
+      phiHist->Fill(branchData[8]);
     }
     
     // Calculate mean and median of deviation distribution
     Double_t deviationMean = deviationHist->GetMean();
     Double_t deviationMedian;
+    Double_t thetaErrorMedian;
+    Double_t phiErrorMedian;
+    
     Double_t medianQuantile = 0.5;
     
     deviationHist->GetQuantiles(1,&deviationMedian,&medianQuantile);
+    thetaErrorHist->GetQuantiles(1,&thetaErrorMedian,&medianQuantile);
+    phiErrorHist->GetQuantiles(1,&phiErrorMedian,&medianQuantile);
     
-    cout << "Median deviation: " << deviationMedian << endl << endl;
+    //cout << "Median deviation: " << deviationMedian << endl << endl;
     
     delete deviationHist;
     
@@ -91,6 +106,8 @@ int main (int argc, char** argv)
     // Save data point
     deviationPlot->SetPoint(pointCounter - 1, pressure, timeRes, deviationMedian);
     deviationPlotMean->SetPoint(pointCounter - 1, pressure, timeRes, deviationMean);
+    thetaErrorPlot->SetPoint(pointCounter - 1, pressure, timeRes, thetaErrorMedian);
+    phiErrorPlot->SetPoint(pointCounter - 1, pressure, timeRes, phiErrorMedian);
   }
   
   TFile* analysisFile = new TFile("/storage/gp_ws_ddm/Analysis.root", "UPDATE");
@@ -100,6 +117,12 @@ int main (int argc, char** argv)
   
   deviationPlotMean->Write();
   delete deviationPlotMean;
+  
+  thetaErrorPlot->Write();
+  delete thetaErrorPlot;
+  
+  phiErrorPlot->Write();
+  delete phiErrorPlot;
   
   steeringFile.close();
   analysisFile->Close();
